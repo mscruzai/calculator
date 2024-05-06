b80431ee475eb7c409c28065bbc8914407ec75d4
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-05-06 11:36:00 +0800

    Finished backspace event function. Fixed parsing of displayed answer using length, toPrecision, truncate, etc.

--- jscript.js
+++ jscript.js
@@ -1,9 +1,9 @@
 const maxScreenLength = 17;
 const overflowMessage = "Screen overflow"
 
-let firstNumber;
+let lastOperand = '';
 let operator;
-let secondNumber;
+let newOperand = '';
 
 function add(a, b) {
     return a + b;
@@ -41,17 +41,30 @@ const numDisplay = document.querySelector(".numeric-line");
 eqDisplay.textContent = '';
 numDisplay.value ='0';
 
-let lastOperand = '';
-let newOperand = '';
 let noFirstDigit = true;
 let calcKey = false;
+let hasPoint = false;
 
 const digitButtons = document.querySelectorAll('[data-type="digit"]');
 
 digitButtons.forEach(button => {
     button.addEventListener('click', () => {
+        if(button.textContent == '.'){
+            if(hasPoint){
+                return;
+            }
+            else {
+                hasPoint = true;
+            }
+        }
         if(noFirstDigit || numDisplay.value == '0' || numDisplay.value == overflowMessage ){
-            numDisplay.value = button.textContent;
+            if(button.textContent == '.'){
+                numDisplay.value = '0' + '.';
+                hasPoint = true;
+            }
+            else {
+                numDisplay.value = button.textContent;
+            }
             noFirstDigit = false;
             if(calcKey) {
                 eqDisplay.textContent = '';
@@ -59,11 +72,11 @@ digitButtons.forEach(button => {
                 calcKey = false;
             }
         }
-        else if(numDisplay.value.length < maxScreenLength){
-            numDisplay.value =  numDisplay.value + button.textContent;
+        else if(numDisplay.value.length > maxScreenLength){
+            numDisplay.value = overflowMessage;
         }
         else {
-            numDisplay.value = overflowMessage;
+            numDisplay.value =  numDisplay.value + button.textContent;
         }
     });
 });
@@ -79,23 +92,27 @@ operatorButtons.forEach(button => {
         if(!noFirstDigit){
             if(lastOperand ==='') {
                 lastOperand = numDisplay.value;    
-                eqDisplay.textContent = lastOperand + newOperator;
+                eqDisplay.textContent = lastOperand + ' ' +  newOperator;
             }
             else {
                 newOperand = numDisplay.value;
                 newOperand = operate(parseFloat(lastOperand), parseFloat(newOperand), operator);
-                numDisplay.value = newOperand;
-                eqDisplay.textContent = newOperand + newOperator;
+                
+                answer = newOperand.toString();
+                numDisplay.value = (answer.length > maxScreenLength) ? answer.slice(0, maxScreenLength): answer;
+                eqDisplay.textContent = newOperand + ' ' + newOperator;
+                
                 lastOperand = newOperand;
 
             }
             noFirstDigit = true;
         }
         else {
-            eqDisplay.textContent = lastOperand + newOperator;
+            eqDisplay.textContent = lastOperand + ' ' + newOperator;
         }
         operator = newOperator;
         calcKey = false;
+        hasPoint = false;
     });
 });
 
@@ -107,21 +124,33 @@ equalsButton.addEventListener('click', () => {
     if(!calcKey){ // If consecutive equal, retain newOperand for next iteration
         newOperand = numDisplay.value; 
     }
-    eqDisplay.textContent = lastOperand + operator + newOperand + '=';
+    eqDisplay.textContent = lastOperand + ' ' + operator + ' ' + newOperand + ' ' + '=';
     answer = operate(parseFloat(lastOperand), parseFloat(newOperand), operator);
+    // lastOperand = answer; // Should we store the original number?
+
+    if(('' + answer).includes('e') ){
+        answer = answer.toPrecision(maxScreenLength -5);
+    } else {
+        answer = answer.toString();
+        answer = (answer.length > maxScreenLength) ?
+                answer.slice(0, maxScreenLength): answer;    
+    }
+
     numDisplay.value = answer;
     lastOperand = answer;
+
     noFirstDigit = true;
     calcKey = true;
+    hasPoint = false;
 });
 
 
 const backspaceButton = document.querySelector('#backspace');
-clearEntryButton.addEventListener('click', () => {
+backspaceButton.addEventListener('click', () => {
     
-    noFirstDigit = true;
-    numDisplay.value = '0';
-
+    if(calcKey) return;
+    let s = numDisplay.value;
+    numDisplay.value = (s.length > 1)? s.slice(0,-1): '0';
 });
 
 const clearEntryButton = document.querySelector('#clear-entry');
@@ -129,6 +158,7 @@ clearEntryButton.addEventListener('click', () => {
     
     noFirstDigit = true;
     numDisplay.value = '0';
+    hasPoint = false;
 
 });
 
@@ -140,7 +170,8 @@ clearAllButton.addEventListener('click', () => {
     eqDisplay.textContent = '';
 
     lastOperand = '';
-    //newOperand = '';
+    //newOperand = ''; I think no need
     calcKey = false;
+    hasPoint = false;
 
 });
